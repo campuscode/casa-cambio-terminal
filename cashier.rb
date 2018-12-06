0efc85b238a97eef441215cfbe2143c426edea46
Henrique Morato <henrique.morato@campuscode.com.br>
2018-12-06 11:40:15 -0200

    Orientação a objetos

---
+++ cashier.rb
@@ -0,0 +1,53 @@
+require_relative 'transaction'
+
+class Cashier
+  attr_accessor :quotation, :dollar_amount, :real_amount, :transactions
+
+  def initialize
+    print 'Digite a cotação do dólar hoje: $ '
+    @quotation = gets.to_f
+    print 'Digite a quantidade de dólar em caixa: $ '
+    @dollar_amount = gets.to_f
+    print 'Digite a quantidade de real em caixa: R$ '
+    @real_amount = gets.to_f
+    @transactions = []
+  end
+
+  def buy_dollars(quantity)
+    transaction = Transaction.new(quantity: quantity, currency: 'dollar',
+                                  quotation: quotation, transaction_type: 'buy')
+    puts "Essa transação vai custar R$ #{transaction.total}"
+    if transaction.confirm?
+      self.dollar_amount += transaction.quantity
+      self.real_amount -= transaction.total
+      transactions << transaction
+      puts 'Operação realizada com sucesso'
+      return
+    end
+    puts 'Operação não realizada'
+  end
+
+  def sell_dollars(quantity)
+    transaction = Transaction.new(quantity: quantity, currency: 'dollar',
+                                  quotation: quotation, transaction_type: 'buy')
+    puts "Essa transação vai custar R$ #{transaction.total}"
+    if transaction.confirm?
+      self.dollar_amount -= transaction.quantity
+      self.real_amount += transaction.total
+      transactions << transaction
+      puts 'Operação realizada com sucesso'
+      return
+    end
+    puts 'Operação não realizada'
+  end
+
+  def to_s
+    <<~HEREDOC
+    Dólar em caixa: $ #{dollar_amount}
+    Real em caixa: R$ #{real_amount}
+    Cotação do dólar: R$ #{quotation}
+    Transação do dia
+    #{transactions.map { |t| t.to_s }.join("\n")}
+    HEREDOC
+  end
+end
--- main.rb
+++ main.rb
@@ -1,75 +1,41 @@
-require_relative 'transaction'
+require_relative 'cashier'
+require 'io/console'
 
 puts 'Bem vindo ao Sistema de Câmbio'
-transactions = []
+cashier = Cashier.new
 
-print 'Digite a cotação do dólar hoje: $ '
-quotation = gets.to_f
-print 'Digite a quantidade de dólar em caixa: $ '
-dollar_amount = gets.to_f
-print 'Digite a quantidade de real em caixa: R$ '
-real_amount = gets.to_f
-
-puts "\n========== Menu =========="
-puts "1 - Comprar Dólar"
-puts "2 - Vender Dólar"
-puts "3 - Balanço"
-puts "4 - Sair"
-print 'Digite a opção desejada: '
-menu = gets.to_i
-
-loop do
-  case menu
-  when 1
-    print 'Qual a quantidade em dólar que deseja incluir no caixa? $ '
-    quantity = gets.to_i
-    transaction = Transaction.new(quantity: quantity, currency: 'dollar',
-                                  quotation: quotation, transaction_type: 'buy')
-    puts "Essa transação vai custar R$ #{transaction.total}"
-    if transaction.confirm?
-      dollar_amount += transaction.quantity
-      real_amount -= transaction.total
-      transactions << transaction
-      puts 'Operação realizada com sucesso'
-    else
-      puts 'Operação não realizada'
-    end
-    puts "\nPressione qualquer tecla para continuar"
-    gets
-  when 2
-    print 'Qual a quantidade em dólar que deseja retirar do caixa? $ '
-    quantity = gets.to_i
-    transaction = Transaction.new(quantity: quantity, currency: 'dollar',
-                                  quotation: quotation,
-                                  transaction_type: 'sell')
-    puts "Essa transação vai incluir R$ #{transaction.total}"
-    if transaction.confirm?
-      dollar_amount -= transaction.quantity
-      real_amount += transaction.total
-      transactions << transaction
-      puts 'Operação realizada com sucesso'
-    else
-      puts 'Operação não realizada'
-    end
-    puts "\nPressione qualquer tecla para continuar"
-    gets
-  when 3
-    puts "\nDólar em caixa: $ #{dollar_amount}"
-    puts "Real em caixa: R$ #{real_amount}"
-    puts "Cotação do dólar: R$ #{quotation}"
-    puts "\nTransação do dia"
-    puts transactions
-  when 4
-    exit 0
-  else
-    puts 'Opção inválida'
-  end
-
+def menu
   puts "\n========== Menu =========="
   puts "1 - Comprar Dólar"
   puts "2 - Vender Dólar"
   puts "3 - Balanço"
   puts "4 - Sair"
   print 'Digite a opção desejada: '
-  menu = gets.to_i
+  gets.to_i
+end
+
+def wait_keyboard
+  puts "\nPressione qualquer tecla para continuar"
+  STDIN.getch
+end
+
+loop do
+  case menu
+  when 1
+    print 'Qual a quantidade em dólar que deseja incluir no caixa? $ '
+    quantity = gets.to_i
+    cashier.buy_dollars(quantity)
+    wait_keyboard
+  when 2
+    print 'Qual a quantidade em dólar que deseja retirar do caixa? $ '
+    quantity = gets.to_i
+    cashier.sell_dollars(quantity)
+    wait_keyboard
+  when 3
+    puts cashier
+  when 4
+    exit 0
+  else
+    puts 'Opção inválida'
+  end
 end
